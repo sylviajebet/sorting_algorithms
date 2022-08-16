7458ee42b49688bc025466c3c18a38a13cf80158
sylviajebet <sylviajebett@gmail.com>
2022-08-17 00:23:39 +0300

    File updated

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,7 +18,7 @@ void selection_sort(int *array, size_t size)
 		min = i; /*First element is min*/
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[jMin])
+			if (array[j] < array[min])
 				min = j;
 		}
 		if (min != i)
